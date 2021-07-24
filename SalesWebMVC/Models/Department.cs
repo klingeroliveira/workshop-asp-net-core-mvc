77ded1a144466c6b605edcf2e35fa679e9cf05b6
KlingerOliveira <KlingerOliveira@hotmail.com>
2021-07-24 15:48:43 -0300

    Other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,9 +1,34 @@
-﻿
+﻿using System;
+using System.Linq;
+
+using System.Collections.Generic;
+
 namespace SalesWebMVC.Models
 {
     public class Department
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int iD, string name)
+        {
+            ID = iD;
+            Name = name;
+        }
+
+        public void AddSeller (Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalDepartment(DateTime inicial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(inicial, final));
+        }
     }
 }
